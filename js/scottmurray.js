bf2c55c5883e76327b9e01adb381268b142d5383
Victor Dibia <chuvidi2003@gmail.com>
2016-08-04 22:54:43 -0400

    add scaling, and axis

--- js/scottmurray.js
+++ js/scottmurray.js
@@ -2,8 +2,7 @@
 var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
 							11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-
-//d3.select("body").append("p").text("New paragraph!");
+// Use data .. print in loop
 d3.select("body")
     .append("div")
     .selectAll("p")
@@ -19,7 +18,8 @@ d3.select("body")
     }
   });
 
-  d3.select("body").selectAll("div")
+// Simple bar chart
+d3.select("body").selectAll("div")
       .data(dataset)
       .enter()
       .append("div")
@@ -29,6 +29,7 @@ d3.select("body")
        return barHeight + "px";
 });
 
+// Simple Circle Chart
 d3.select("body")
   .append("div")
   .append("svg")
@@ -47,6 +48,7 @@ d3.select("body")
        })
   .attr("cy",30);
 
+// Bar Chart with label
 h = 100 ;
 w = 500 ; var barPadding = 1;
   barsvg = d3.select("body")
@@ -70,6 +72,7 @@ w = 500 ; var barPadding = 1;
    }).attr("fill", function(d) {
 					return "rgb(0, 0, " + (d * 10) + ")";
 			   });
+  // draw text label on bar chart
   barsvg.selectAll("text")
           .data(dataset)
           .enter()
@@ -88,25 +91,43 @@ w = 500 ; var barPadding = 1;
 
  var dataset = [
                  [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-                 [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+                 [410, 12], [475, 44], [25, 67], [85, 21], [220, 88], [600, 150]
                        ];
+// Simple scatter plot
+d3.select("body")
+  .append("div")
+  .attr("style", "padding: 10px")
+  .append("text")
+  .text(" Scale : using latest d3 syntax .. d3.scaleLinear() , d3.axisLeft(scale), d3.axistRight(scale)")
+
+h = 300 ;
  plotsvg = d3.select("body")
    .append("div")
    .append("svg")
    .attr("height", h)
    .attr("width", w);
+   // Working with Scales http://alignedleft.com/tutorials/d3/scales
+var padding = 20;
+var xscale = d3.scaleLinear().domain([0, d3.max(dataset, function(d) { return d[0]; })]).range([padding, w - padding*2]);
+var yscale = d3.scaleLinear().domain([0, d3.max(dataset, function(d) { return d[1]; })]).range([h-padding, padding]);
+var rscale = d3.scaleLinear().domain([0, d3.max(dataset, function(d) { return d[1]; })]).range([2, 10]);
+var xaxis = d3.axisTop(xscale).ticks(5);
+var yaxis = d3.axisLeft(yscale).ticks(5);
 
 plotsvg.selectAll("circle")
     .data(dataset)
     .enter()
     .append("circle")
     .attr("cx", function(d){
-      return d[0] ;
+      return xscale(d[0]) ;
     })
     .attr("cy", function(d){
-      return d[1] ;
+      return yscale(d[1]) ;
     })
-    .attr("r", 5)
+    .attr("r", function(d) {
+			   		return rscale(d[1]);
+		})
+
 
 plotsvg.selectAll("text")
        .data(dataset)
@@ -116,10 +137,18 @@ plotsvg.selectAll("text")
          return (d[0] + "," + d[1])
        })
        .attr("x", function(d,i){
-         return d[0];
+         return xscale(d[0]);
        }).attr("y", function(d){
-         return d[1];
+         return yscale(d[1]);
        }).attr("font-family", "sans-serif")
       .attr("font-size", "11px")
       .attr("fill", "green")
       .attr("text-anchor", "middle")
+plotsvg.append("g")
+      .attr("class","axis")
+      .attr("transform", "translate(0," + (h - padding) + ")")
+      .call(xaxis)
+  plotsvg.append("g")
+        .attr("class","axis")
+        .attr("transform", "translate(" + (padding) + ",0 )")
+        .call(yaxis)
---
+++ router.js
@@ -0,0 +1,6 @@
+var config = require("./config")
+var serveStatic = require('serve-static');
+var connect = require('connect');
+var app = connect().use(serveStatic(__dirname)).listen(config.portnumber, function(){
+    console.log('Server running on '+ config.portnumber + ' ...');
+});
